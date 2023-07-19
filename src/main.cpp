30a7709909cb123c973b38f9a74803711882a356
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-07-19 17:05:00 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include <ros.h>
 
+#include "PS3I2C.h"
 #include "NavitonROS.h"
 
 ros::NodeHandle nh;
 
+PS3I2C ps3_wireless(0x73);
+PS3I2C ps3_wired(0x74);
 NavitonROS nvt;
 
 void setup()
@@ -13,11 +16,19 @@ void setup()
     digitalWrite(LED_BUILTIN, HIGH);
 
     nh.initNode();
+
+    ps3_wireless.Init();
+    ps3_wired.Init();
+
     nvt.Init(nh);
 }
 
 void loop()
 {
+    ps3_wireless.Update();
+    ps3_wired.Update();
+
     nvt.Update();
+    
     nh.spinOnce();
 }
