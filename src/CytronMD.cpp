ffc1a381671b1fec8c73f0c653ed64dd89d9d474
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-07-19 22:20:52 +0900

    Update CytronMD.cpp

--- src/CytronMD.cpp
+++ src/CytronMD.cpp
@@ -1,15 +1,22 @@
 #include "CytronMD.h"
 
+/// @brief 
 CytronMD::CytronMD()
 {
 
 }
 
+/// @brief 
+/// @param pin_pwm 
+/// @param pin_dir 
 CytronMD::CytronMD(int pin_pwm, int pin_dir)
 {
     Attach(pin_pwm, pin_dir);
 }
 
+/// @brief 
+/// @param pin_pwm 
+/// @param pin_dir 
 void CytronMD::Attach(int pin_pwm, int pin_dir)
 {
     _pin_pwm = pin_pwm;
@@ -20,6 +27,8 @@ void CytronMD::Attach(int pin_pwm, int pin_dir)
     digitalWrite(_pin_dir, LOW);
 }
 
+/// @brief 
+/// @param power -1.0~1.0
 void CytronMD::Drive(double power)
 {
     power = constrain(power, -1.0, 1.0);
@@ -28,11 +37,14 @@ void CytronMD::Drive(double power)
     digitalWrite(_pin_dir, power < 0 ? LOW : HIGH);
 }
 
+/// @brief 
+/// @param pulse 1000~2000
 void CytronMD::WriteMicroseconds(int pulse)
 {
     Drive(map(pulse, 1000, 2000, -1.0, 1.0));
 }
 
+/// @brief 
 void CytronMD::Stop()
 {
     Drive(0.0);
