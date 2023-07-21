414a122be76025d71cf663928f0ed8aed4519378
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-07-21 19:36:11 +0900

    Update params

--- include/config/SoftwareParams.h
+++ include/config/SoftwareParams.h
@@ -6,10 +6,10 @@
 #define WHEEL_LOOP_TIME                     0.05
 
 #define WHEEL_MAX_POWER                     1.0
-#define WHEEL_MAX_ACCELERATION              1.0
+#define WHEEL_MAX_ACCELERATION              4.0
 
-#define WHEEL_PID_GAIN                      2.0,0.0,0.0
-#define WHEEL_PID_AW_GAIN                   0.0
-#define WHEEL_PID_DERIATIVE_FILTER_COEF     0.0
+#define WHEEL_PID_GAIN                      2.0,5.0,0.0
+#define WHEEL_PID_AW_GAIN                   1.0
+#define WHEEL_PID_DERIATIVE_FILTER_COEF     0.01
 
 #endif
