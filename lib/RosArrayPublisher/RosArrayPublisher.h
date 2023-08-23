0abf4828353a73badb9101423169f91ee8578e10
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-08-23 17:06:24 +0900

    Update RosArrayPublisher

--- lib/RosArrayPublisher/RosArrayPublisher.h
+++ lib/RosArrayPublisher/RosArrayPublisher.h
@@ -16,16 +16,36 @@ class RosArrayPublisher
 {
     public:
         /// @brief 空コンストラクラ
-        RosArrayPublisher();
+        RosArrayPublisher() : _array_pub("", &array)
+        {
+        }
 
         /// @brief コンストラクタ
         /// @param nh rosのノードハンドル
         /// @param topic_name publishするtopic名
         /// @param array_size publishする配列の要素数
-        RosArrayPublisher(ros::NodeHandle& nh,const char *topic_name, int array_size);
+        RosArrayPublisher(const char *topic_name, int array_size) :
+        _array_pub(topic_name, &array)
+        {
+            array.data = decltype(array.data)(malloc(sizeof(array.data) * array_size));
+            array.data_length = array_size;
+            for(int i = 0; i < array_size; i++)
+            {
+                array.data[i] = 0.0;
+            }
+        }
+
+        /// @brief advertiseする関数
+        void Init(ros::NodeHandle& nh)
+        {
+            nh.advertise(_array_pub);
+        }
 
         /// @brief publishする関数
-        void Publish();
+        void Publish()
+        {
+            _array_pub.publish(&array);
+        }
 
         /// @brief publishするデータを入れるオブジェクト array.data[1]のようにアクセスする
         T array;
