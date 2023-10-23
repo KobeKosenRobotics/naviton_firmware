a83c31f1a5836f9572184249577db316af5ab53f
sou46 <r119163@g.kobe-kosen.ac.jp>
2023-10-23 18:23:05 +0900

    Update manual param

--- include/config/ManualParams.h
+++ include/config/ManualParams.h
@@ -1,9 +1,9 @@
 #ifndef MANUAL_PARAMS_H
 #define MANUAL_PARAMS_H
 
-#define DEAD_ZONE_PERCENTAGE    0.1     // []
+#define DEAD_ZONE_PERCENTAGE    0.2     // []
 
 #define MAX_LINEAR_VELOCITY     1.0     // [m/s]
-#define MAX_ANGULAR_VELOCITY    3.0     // [rad/s]
+#define MAX_ANGULAR_VELOCITY    1.0     // [rad/s]
 
 #endif
