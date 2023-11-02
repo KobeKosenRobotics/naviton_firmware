7de3909ff9ca6a8006920fb822926e52d9379286
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-11-02 14:36:19 +0900

    Update docs

--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['init_0',['init',['../class_differential_drive.html#a3249661138416fb98bd3327e0e79a72a',1,'DifferentialDrive::Init()'],['../class_gyro.html#a7ae704cad1bf8fbfbd0b54b255e5a603',1,'Gyro::Init()'],['../class_p_s3_i2_c.html#aabe441169537f6443f7a6bd7970eab0d',1,'PS3I2C::Init()'],['../class_wheel.html#a37de93673963de8d24843e471f516f76',1,'Wheel::Init()'],['../class_ros_array_publisher.html#ad27d47e222f61a36d87adef772497887',1,'RosArrayPublisher::Init()']]],
+  ['init_0',['init',['../class_differential_drive.html#a3180d36913b354f22d1aa32f11ee0434',1,'DifferentialDrive::Init()'],['../class_gyro.html#a7ae704cad1bf8fbfbd0b54b255e5a603',1,'Gyro::Init()'],['../class_p_s3_i2_c.html#aabe441169537f6443f7a6bd7970eab0d',1,'PS3I2C::Init()'],['../class_wheel.html#a73a90efc6a029647cd272f1408cbe235',1,'Wheel::Init()'],['../class_ros_array_publisher.html#ad27d47e222f61a36d87adef772497887',1,'RosArrayPublisher::Init()']]],
   ['isconnected_1',['IsConnected',['../class_p_s3_i2_c.html#a65a6d333d9fa94e0d36b9eae39692420',1,'PS3I2C']]]
 ];
