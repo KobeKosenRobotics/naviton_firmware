89d81d0aa8ee64f7f838a10f99d986a08575a3f8
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-11-22 00:47:15 +0900

    Align params

--- include/config/HardwareParams.h
+++ include/config/HardwareParams.h
@@ -1,11 +1,11 @@
 #ifndef HARDWARE_PARAMS_H
 #define HARDWARE_PARAMS_H
 
-#define I2C_SCL 33
-#define I2C_SDA 34
+#define I2C_SCL                 33
+#define I2C_SDA                 34
 
 #define PS3_WIRELESS_ADDRESS    0x73
-#define PS3_WIRED_ADDRESS      0x74
+#define PS3_WIRED_ADDRESS       0x74
 
 #define BNO_ID                  -1
 #define BNO_ADDRESS             0x28
@@ -15,13 +15,12 @@
 #define MANUAL_SWITCH_PIN       10
 
 // ENCODER_ALPHA, ENCODER_BETA, MD_PWM, MD_DIR
-#define LEFT_WHEEL_PINS    15,16,22,23
-#define RIGHT_WHEEL_PINS     36,37,20,21
+#define LEFT_WHEEL_PINS         15,16,22,23
+#define RIGHT_WHEEL_PINS        36,37,20,21
 
+#define FOOTPRINT_WIDTH         0.53
+#define WHEEL_DIAMETER          0.315
 
-#define FOOTPRINT_WIDTH 0.53
-#define WHEEL_DIAMETER 0.315
-
-#define WHEEL_ENCODER_PPR 8192.0
+#define WHEEL_ENCODER_PPR       8192.0
 
 #endif
