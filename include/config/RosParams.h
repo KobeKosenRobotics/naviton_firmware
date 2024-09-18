60cf0317f875e4957c9a6e22a04fba1a2caf6006
SoshiroMaruoka <r119163@g.kobe-kosen.ac.jp>
2024-09-18 18:20:30 +0900

    update rosparams.h

--- include/config/RosParams.h
+++ include/config/RosParams.h
@@ -1,10 +1,11 @@
 #ifndef ROS_PARAMS_H
 #define ROS_PARAMS_H
 
-#define CMD_VEL_TOPIC   "/naviton/mobile_base_controller/cmd_vel"
-#define REMOTE_VEL_TOPIC   "/mqtt/remote_controller/remote_vel"
-#define REMOTE_CMD_TOPIC   "/mqtt/remote_switch/remote_cmd"
+#define CMD_VEL_TOPIC       "/naviton/mobile_base_controller/cmd_vel"
+#define REMOTE_VEL_TOPIC    "/mqtt/remote_controller/cmd_vel"
+#define REMOTE_EMG_TOPIC    "/mqtt/remote_controller/emg_swtch"
+#define REMOTE_MODE_TOPIC   "/mqtt/remote_controller/mode_swtch"
 
-#define STATE_TOPIC     "/naviton/firmware/teensy/teensy_data"
+#define STATE_TOPIC         "/naviton/firmware/teensy/teensy_data"
 
 #endif
